296f649f1ef314edd7ebf8f1081dc30a47f4873f
Cleônides <cleonidesn@gmail.com>
2024-09-28 13:56:33 -0300

    classe user

---
+++ src/main/java/br/com/dicasdeumdev/api/domain/User.java
@@ -0,0 +1,26 @@
+package br.com.dicasdeumdev.api.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
+}
